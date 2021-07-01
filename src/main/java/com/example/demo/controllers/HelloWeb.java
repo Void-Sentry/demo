ac3e04fba4f64308d6de9341d7f968426dd5532e
Void-Sentry <oseias.tv.101@gmail.com>
2021-07-01 20:26:40 -0300

    update

--- src/main/java/com/example/demo/controllers/HelloWeb.java
+++ src/main/java/com/example/demo/controllers/HelloWeb.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import java.io.IOException;
 @RequestMapping("/hello")
 public class HelloWeb {
 
+    @GetMapping
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         response.getWriter().println("hello, eu sou lindo");
     }
